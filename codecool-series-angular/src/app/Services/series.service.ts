abb7ee2969b2d8d84c4b484a2fdcd453db1c1e0d
Kiss Bence <kissbence19@gmail.com>
2022-11-03 11:10:24 +0100

    Refactor: Service methods return Observables

--- codecool-series-angular/src/app/Services/series.service.ts
+++ codecool-series-angular/src/app/Services/series.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Show } from '../Models/Show';
 
 @Injectable({
@@ -9,11 +10,11 @@ export class SeriesService {
 
   constructor(private http:HttpClient) { }
 
-  getAllShows(){
+  getAllShows():Observable<Show[]>{
     return this.http.get<Show[]>(`/showapi/shows`);
   }
 
-  getShowbyId(id:string){
+  getShowbyId(id:string):Observable<Show>{
     return this.http.get<Show>(`/showapi/shows/${id}`);
   }
 
