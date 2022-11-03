c78b80b92d8962c865790221587175003bb47507
Kiss Bence <kissbence19@gmail.com>
2022-11-03 11:12:14 +0100

    moving service calls in series-details to method

--- codecool-series-angular/src/app/series-detail/series-detail.component.ts
+++ codecool-series-angular/src/app/series-detail/series-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Show } from '../Models/Show';
 import { SeriesService } from '../Services/series.service';
 
@@ -16,11 +17,13 @@ export class SeriesDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.showId = this.route.snapshot.params['showId'];
+    this.getShow();
+  }
+
+  getShow(){
     this.seriesService.getShowbyId(this.showId).subscribe(result => {
       this.show = result as Show;
       this.show.trailer = this.show.trailer?.replace("http", "https").replace("watch?v=", "embed/");
     }, error => console.error(error));
-
   }
-
 }
