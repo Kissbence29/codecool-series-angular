a815f474fc3499d099e8dcd9056837637cd4c173
Kiss Bence <kissbence19@gmail.com>
2022-11-07 15:16:26 +0100

    creating top-rated component

---
+++ codecool-series-angular/src/app/top-rated/top-rated.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Show } from '../Models/Show';
+import { SeriesCardComponent } from '../series-card/series-card.component';
+import { SeriesService } from '../Services/series.service';
+
+@Component({
+  selector: 'app-top-rated',
+  templateUrl: './top-rated.component.html',
+  styleUrls: ['./top-rated.component.css']
+})
+export class TopRatedComponent implements OnInit {
+
+  public shows:Show[]=[];
+  constructor(private seriesService:SeriesService,private seriesCard:SeriesCardComponent) { }
+
+  ngOnInit(): void {
+    this.getShows();
+  }
+
+  private getShows()
+  {
+    this.seriesService.getTopHundred().subscribe(result=>
+    {
+        this.shows=result as Show[];
+
+    },error=>console.error(error)
+    )
+  }
+
+  onClick(page:any){
+    this.seriesCard.navToSerDetail(page);
+  }
+
+}
