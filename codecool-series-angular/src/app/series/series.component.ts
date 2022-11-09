f1c9dbd21dcfe83ff32d4fedc4e5fee1918a96af
Kiss Bence <kissbence19@gmail.com>
2022-11-09 15:53:56 +0100

    Improving search on main page by searching with genres

--- codecool-series-angular/src/app/series/series.component.ts
+++ codecool-series-angular/src/app/series/series.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SeriesService } from '../Services/series.service';
 import { Show } from '../Models/Show';
 import { SeriesCardComponent } from '../series-card/series-card.component';
+import { Genre } from '../Models/Genre';
 @Component({
   selector: 'app-series',
   templateUrl: './series.component.html',
@@ -13,22 +14,35 @@ export class SeriesComponent implements OnInit {
   private baseShows: Show[] = [];
   p: number = 1;
   count: number = 9;
-  constructor(private seriesService: SeriesService,private router: Router,private child:SeriesCardComponent) {
+  public genres: Genre[] = [];
+  private selectedGenre: string = "";
+  private sortedShow: Show[] = [];
+  constructor(private seriesService: SeriesService, private router: Router, private child: SeriesCardComponent) {
   }
 
   onChange(event: Event) {
-    this.shows = this.baseShows;
-    this.shows = this.shows.filter((show: Show) => show.title?.toLowerCase().includes((event.target as HTMLInputElement).value))
-
-    if ((event.target as HTMLInputElement).value === '') {
+    var input = (event.target as HTMLInputElement).value;
+    if (this.selectedGenre == "" && input == "") {
       this.shows = this.baseShows;
-
     }
+    else if ((this.selectedGenre !== "" && input == "") || (this.selectedGenre !== "" && input !== "")) {
+      this.shows = this.sortedShow;
+    }
+
+    else if (this.selectedGenre == "" && input != "") {
+      this.shows = this.baseShows;
+    }
+    this.filterByTitle(input);
+  }
+
+  private filterByTitle(input: string) {
+    this.shows = this.shows.filter(show => show.title?.toLowerCase().includes(input));
   }
 
   ngOnInit(): void {
-   this.getShows();
-    
+    this.getShows();
+    this.getGenres();
+
   }
 
   getShows() {
@@ -39,9 +53,40 @@ export class SeriesComponent implements OnInit {
     )
   }
 
+  getGenres() {
+    this.seriesService.getAllGenre().subscribe(result => {
+      this.genres = result;
+    }
+    );
+  }
 
-  
+  selectGenre(value: string) {
+    this.selectedGenre = value;
+    this.filterShowsByGenre();
 
+  }
+
+  filterShowsByGenre() {
+    if (this.selectedGenre == "") {
+      this.shows = this.baseShows;
+    }
+    else {
+      this.shows = [];
+      this.sortedShow = [];
+      for (let show of this.baseShows) {
+        if (show.showGenres?.includes(this.selectedGenre)) {
+          this.shows.push(show);
+          this.sortedShow.push(show);
+        }
+      }
+      console.log(this.sortedShow.length)
+    }
+  }
+
+  noResult()
+  {
+    return this.shows.length!==0;
+  }
 }
 
 
