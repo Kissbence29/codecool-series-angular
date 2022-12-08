357b827c9a9bacc29509c915df54b107cd8189e2
Kiss Bence <kissbence19@gmail.com>
2022-12-08 15:09:49 +0100

    implementing season changing button

--- codecool-series-angular-frontend/src/app/series-season/series-season.component.ts
+++ codecool-series-angular-frontend/src/app/series-season/series-season.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {  AfterViewChecked, Component,Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Season } from '../Models/Season';
 import { SeriesService } from '../Services/series.service';
@@ -9,28 +9,35 @@ import { SeriesService } from '../Services/series.service';
   templateUrl: './series-season.component.html',
   styleUrls: ['./series-season.component.css']
 })
-export class SeriesSeasonComponent implements OnInit {
-
+export class SeriesSeasonComponent implements OnInit,AfterViewChecked {
+  private button!: HTMLElement;
   constructor(private route:ActivatedRoute,private seriesService:SeriesService) { }
+  ngAfterViewChecked(): void {
+   this.button =  (document.querySelector("button")as HTMLElement);
+   if(this.button.innerText.split(" ")[1]==="1"){this.button.classList.add("focused")};
+   if(this.button.classList.contains("notagain")){this.button.classList.remove("focused")};
+  }
   seasons:Season[]=[];
-  baseSeasons:Season[]=[];
   showId:string='';
   @Input()
   seasonNumber:number=1;
   selectedSeason:Season={};
-  
   ngOnInit(): void
   { 
     this.showId = this.route.snapshot.params['showId'];
     this.getSeasonsByShowid(this.showId);
+    
     }
+
+    
+  
 
   getSeasonsByShowid(showId:string)
   {
     this.seriesService.getSeasonsByShowId(showId).subscribe(result=>
       {
         this.seasons = result as Season[];
-        this.baseSeasons = result as Season[];
+      
         this.getSeasonBySeasonNumber(this.seasonNumber);
       },error=>console.error(error));
       
@@ -38,8 +45,11 @@ export class SeriesSeasonComponent implements OnInit {
 
   selectSeason(seasonNumber:Event)
   {
+      this.button.classList.remove("focused");
+      this.button.classList.add("notagain");
       this.seasonNumber = +(seasonNumber.target as HTMLInputElement).value;
       this.getSeasonBySeasonNumber(this.seasonNumber);
+      
   }
 
   getSeasonBySeasonNumber(seasonNumber:number)
@@ -49,6 +59,6 @@ export class SeriesSeasonComponent implements OnInit {
 
   isSeason()
   {
-   return this.selectedSeason!==undefined&&this.baseSeasons.length!==0;
+   return this.selectedSeason!==undefined;
   }
 }
